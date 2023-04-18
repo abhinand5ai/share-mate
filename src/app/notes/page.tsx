176b5eb5e1154ce94e15156851e3fc68f93b7765
abhinand <abhinandsainasari@gmail.com>
2023-04-18 17:06:30 -0500

    moving to codespaces

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,31 +1,51 @@
-import Link from "next/link";
-import { getNotes, Note } from "../../database/notes";
-import { ListResult } from "pocketbase";
+// import PocketBase from 'pocketbase';
+import Link from 'next/link';
+import styles from './Notes.module.css';
+import CreateNote from './CreateNote';
+import { RideFactoryABI } from '@/ethereum/factory';
 
+// export const dynamic = 'auto',
+//   dynamicParams = true,
+//   revalidate = 0,
+//   fetchCache = 'auto',
+//   runtime = 'nodejs',
+//   preferredRegion = 'auto'
+
+
+async function getNotes() {
+    // const db = new PocketBase('http://127.0.0.1:8090');
+    // const result = await db.records.getList('notes');
+    const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
+    const data = await res.json();
+    return data?.items as any[];
+}
 
 export default async function NotesPage() {
     const notes = await getNotes();
+    console.log(RideFactoryABI)
     return (
         <div>
             <h1>Notes</h1>
-            <Notes notes={notes} />
+            <div className={styles.grid}>
+                {notes?.map((note) => {
+                    return <Note key={note.id} note={note} />;
+                })}
+            </div>
+            <CreateNote />
         </div>
     );
 }
 
-function Notes({ notes }: { notes: ListResult<Note> }) {
+function Note({ note }: any) {
+    const { id, title, content, created } = note || {};
+
     return (
-        <ul>
-            {
-                notes.items.map((note) => (
-                    <li key={note.id}>
-                        <Link href={`/notes/${note.id}`}>
-                            {note.title}
-                        </Link>
-                        <p>{note.content}</p>
-                    </li>
-                ))
-            }
-        </ul>
+        <Link href={`/notes/${id}`}>
+            <div className={styles.note}>
+                <h2>{title}</h2>
+                <h5>{content}</h5>
+                <p>{created}</p>
+            </div>
+        </Link>
     );
 }
